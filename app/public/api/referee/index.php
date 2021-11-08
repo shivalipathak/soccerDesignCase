f9bf94b16953d85ad3cac6c0a0e55e58a5bec3a0
Shivali Pathak <42498941+shivalipathak@users.noreply.github.com>
2021-11-07 23:34:58 -0500

    update

--- app/public/api/referee/index.php
+++ app/public/api/referee/index.php
@@ -6,7 +6,7 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM book';
+$sql = 'SELECT * FROM referee';
 $vars = [];
 
 // if (isset($_GET['guid'])) {
@@ -18,10 +18,10 @@ $vars = [];
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$books = $stmt->fetchAll();
+$referees = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($books, JSON_PRETTY_PRINT);
+$json = json_encode($referees, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
---
+++ app/public/js/referee.js
@@ -0,0 +1,105 @@
+const refereeApp = {
+    data() {
+        return {
+            result: undefined,
+            referees: [],
+            app:0,
+            refForm: {},
+            selectedRef: null,
+        }
+    },
+    methods: {
+        fetchRefereeData() {
+            fetch('/api/referee/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.referees = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+        },
+        postNewRef(evt) {       
+
+            fetch('/api/referee/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.refForm),
+                headers: {
+                "Content-Type": "application/json; charset=utf-8"
+                }
+            })
+            .then( response => response.json() )
+            .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referees = json;
+            
+                // reset the form
+                this.refForm = {};
+            });
+        },
+        postRef(evt) {
+            if (this.selectedRef === null) {
+                this.postNewRef(evt);
+            } else {
+                this.postEditRef(evt);
+            }
+          },
+        postEditRef(evt) {
+            fetch('api/referee/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.refForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referees = json;
+    
+                // reset the form
+                this.resetRefForm();
+              });
+          },
+        postDeleteRef(ref) {
+            if (!confirm("Are you sure you want to delete the referee record?")) {
+              return;
+            }
+            console.log("Delete!", ref);
+    
+            fetch('api/referee/delete.php', {
+                method:'POST',
+                body: JSON.stringify(ref),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.referees = json;
+    
+                // reset the form
+                this.resetRefForm();
+              });
+          },
+          selectedRefToEdit(ref) {
+            this.selectedRef = ref;
+            this.refForm = Object.assign({}, this.selectedRef);
+          },
+          resetRefForm() {
+              this.selectedRef = null;
+              this.refForm = {};
+          }
+    },
+    created() {
+        this.fetchRefereeData();
+    }
+}
+  
+Vue.createApp(refereeApp).mount('#refereeApp');
+
